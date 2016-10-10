2ce15bb6b85394a0977c0560fd4b78208f8340b1
Joe McCann <joseph.isaac@gmail.com>
2016-10-10 09:01:10 -0700

    adding user preferences, context menu and json storage.

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // Main application code
 
-const { app, Tray, BrowserWindow, globalShortcut, ipcMain } = require('electron')
+const { app, Tray, BrowserWindow, globalShortcut, ipcMain, Menu } = require('electron')
   , path = require('path')
+  , AutoLaunch = require('auto-launch')
+  , storage = require('electron-json-storage')
   , assetsDirectory = path.join(__dirname, 'assets')
 
 // For dev...
 // require('electron-debug')({showDevTools: true});
 
 // Establish objects...
-let tray = window = undefined
+let tray = window = appAutoLauncher = undefined 
 
 // Don't show the app in the doc, since it's a tray app
 app.dock.hide()
@@ -18,6 +20,8 @@ app.on('ready', () => {
   createTray()
   createWindow()
   registerGlobalShortcuts()
+  registerAutoLauncher()
+  initializeUserPreferences()
 })
 
 // Quit the app when the window is closed
@@ -27,8 +31,9 @@ app.on('window-all-closed', () => {
 
 // Create it
 const createTray = () => {
+  
   tray = new Tray(path.join(assetsDirectory, 'ns-icon.png'))
-  tray.on('right-click', toggleWindow)
+  
   tray.on('double-click', toggleWindow)
   tray.on('click', function (event) {
     toggleWindow()
@@ -38,6 +43,9 @@ const createTray = () => {
       window.openDevTools({mode: 'detach'})
     }
   })
+
+  tray.on('right-click', toggleContextMenu)
+
 }
 
 // Center it
@@ -82,6 +90,51 @@ const createWindow = () => {
   })
 }
 
+// Toggle the context menu
+const toggleContextMenu = () => {
+
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Run at Startup', type: 'radio', click: toggleRunAtStartup},
+    {type: 'separator'},
+    {label: 'Quit', type: 'normal', click: quitAppContextMenuListener }
+  ])
+  tray.setContextMenu(contextMenu)
+  tray.popUpContextMenu([contextMenu])
+
+}
+
+// Quit app if they Quit label is pressed.
+const quitAppContextMenuListener = (menuItem, browserWindow, event) => {
+  if( menuItem.label === 'Quit') app.quit()
+}
+
+// Setting to enable npm search bar to run at startup
+const toggleRunAtStartup = (menuItem, browserWindow, event) => {
+  if( menuItem.label === 'Run at Startup') {
+
+    storage.get('userPreferences', function(object) {
+
+        if (object.isAutoLaunchEnabled) {
+          // Then disable it
+          object.isAutoLaunchEnabled = false
+          appAutoLauncher.disable()
+          storage.set('userPreferences', object, (err) => {
+            if(err) console.error(err)
+          }) // end set userPrefs
+        } // end if
+        else {
+          // Then enable it
+          object.isAutoLaunchEnabled = true
+          appAutoLauncher.enable()
+          storage.set('userPreferences', object, (err) => {
+              if(err) console.error(err)
+            }) // end set userPreferences
+        } // end else
+      }) // end get userPreferences
+  } // end if
+
+} // end toggleRunAtStartup()
+
 // Toggle it
 const toggleWindow = () => {
   if (window.isVisible()) {
@@ -112,4 +165,29 @@ const registerGlobalShortcuts = () => {
   if (!ret) {
     console.error('CommandOrControl+Shift+/ keyboard registration failed.')
   }
+}
+
+const registerAutoLauncher = () => {
+  
+  appAutoLauncher = new AutoLaunch({
+    name: 'NPM Search Bar',
+    path: '/Applications/npm-search-bar.app'
+  })
+
+}
+
+const initializeUserPreferences = () => {
+
+  let initialConfig = {isAutoLaunchEnabled: false}
+
+  storage.has('userPreferences', function(error, hasKey) {
+    if (error) console.error(error)
+
+    if (!hasKey) {
+      storage.set('userPreferences', initialConfig, function(err){
+        if(err) console.error(err)
+        console.log('Initialized userPreferences in storage.')
+      }) // end set
+    }
+  })  // end storage.has
 }
